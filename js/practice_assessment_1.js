cd7695d3fb461f9643fe04ca1964c1702dea00c4
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-06-20 22:08:43 -0500

    Practice Assessment 1 for JS

--- js/practice_assessment_1.js
+++ js/practice_assessment_1.js
@@ -1,5 +1,12 @@
 /**
 
+ This is the first Practice Assessment given for JavaScript!
+ Saturday, June 20, 2020
+
+ */
+
+/**
+
 1)
 Define a function named isTrue that takes in any input and returns true if the input provided is exactly equal to true in value and data type.
 
@@ -176,17 +183,135 @@ Define a function named isTrue that takes in any input and returns true if the i
 
  */
 
-let isIdentical = function (arg1, arg2) {
+// let isIdentical = function (arg1, arg2) {
+//
+//     if (arg1 === arg2) {
+//         console.log("true");
+//         return true;
+//     }
+//     else {
+//         console.log("false");
+//         return false;
+//     }
+//
+// }
+//
+// isIdentical("javascript", "java");
 
-    if (arg1 === arg2) {
-        console.log("true");
-        return true;
-    }
-    else {
-        console.log("false");
-        return false;
-    }
+/**
 
-}
+ 7)
+ Define a function named isEqual that takes in two input arguments. If each argument is equal only in value, then return true. Otherwise return false.
 
-isIdentical(1, "1");
+ isEqual(3, "3")                       // true
+ isEqual("abc123", "abc123")           // true
+ isEqual(true, 1)                      // true
+ isEqual(0, false)                     // true
+ isEqual(4, -5)                        // false
+ isEqual("java", "javascript")         // false
+
+ */
+
+// let isEqual = function (arg1, arg2) {
+//
+//     if (arg1 == arg2) {
+//         console.log("true");
+//         return true;
+//     }
+//     else {
+//         console.log("false");
+//         return false;
+//     }
+//
+// }
+//
+// isEqual("java", "javascript")
+
+
+/**
+
+ 8)
+ Define a function named or that takes in two input arguments. The output returned should be the result of an or operation on both inputs.
+
+ or(true, true)                    // true
+ or(true, false)                   // true
+ or(false, true)                   // true
+ or(false, false)                  // false
+ or("hello", "world")
+
+ */
+
+// let or = function (arg1, arg2) {
+//
+//     if (arg1 === true || arg2 === true) {
+//         console.log("true");
+//         return true;
+//     }
+//     else {
+//         console.log("false");
+//         return false;
+//     }
+//
+// }
+//
+// or("hello", "world");
+
+
+/**
+
+ 9)
+ Define a function named and that takes in two input arguments and returns the result of a logical and operation of both inputs.
+
+ and(true, true)                    // true
+ and(true, false)                   // false
+ and(false, true)                   // false
+ and(false, false)                  // false
+ and("hello", "world")
+
+ */
+
+// let and = function (arg1, arg2) {
+//
+//     if (arg1 === true && arg2 === true) {
+//         console.log("true");
+//         return true;
+//     }
+//     else {
+//         console.log("false");
+//         return false;
+//     }
+//
+// }
+//
+// and("hello", "world");
+
+
+/**
+
+ 10)
+ Define a function named concat that takes in two input arguments. If both arguments are strings, then return the concatenated result. If two numbers are provided, then return the string concatenation of each set of numerals.
+
+ concat("code", "up")                // "codeup"
+ concat("connect", 4)                // "connect4"
+ concat("hello", "world")            // "helloworld"
+ concat(4, 2)                        // "42"
+ concat(true, true)
+
+ */
+
+// let concat = function (arg1, arg2) {
+//
+//     let value = "";
+//
+//     if (arg1 && arg2 && arg1 !== true && arg2 !== true) {
+//         value = (arg1 + "") + arg2;
+//         console.log(value);
+//         return value;
+//     }
+//     else if (arg1 === true && arg2 === true) {
+//         return;
+//     }
+//
+// }
+//
+// concat(true, true);
