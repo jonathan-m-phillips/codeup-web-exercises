92c3e056a20b48e933fca8ef0a60bf023ddcb357
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-07-31 16:00:36 -0500

    promises exercise, complete

--- js/promises.js
+++ js/promises.js
@@ -1,14 +1,37 @@
 "use strict";
 
+
+// taking the API from Github to get a users last commit time but passing the username into the function
 const myPromiseFunction = function (username) {
     let url = `https://api.github.com/users/${username}/events/public`;
-    const githubPromise = fetch(url, {headers: {'Authorization': PERSONAL_ACCESS_KEY}});
-
-    // githubPromise.then(response => response.json()).then(data => console.log(data[0].created_at));
-    // githubPromise.catch(error => console.error(error));
+    let githubPromise = fetch(url, {headers: {'Authorization': PERSONAL_ACCESS_KEY}})
     return githubPromise;
 }
 
 
-let myPromise = myPromiseFunction('tim');
+let myPromise = myPromiseFunction('jonathan-m-phillips');
 myPromise.then(response => response.json()).then(data => console.log(data[0].created_at));
+
+
+
+
+// setting a timeout inside of promise to occur whenever the function reaches a set amount of time set in the parameter
+let wait = function (time) {
+
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            if (time === 1000) {
+                resolve(alert("One second"));
+            } else if (time === 3000) {
+                resolve(alert("Three seconds"));
+            } else {
+                reject(alert("Error"));
+            }
+        }, time);
+        console.log(time)
+    });
+}
+
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
